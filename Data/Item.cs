5b0a0e8ad887134241a1ec565201796a05b534a2
jordan nye <jordannye@hotmail.com>
2024-02-28 20:05:20 +0000

    Embed pretty command

--- Data/Item.cs
+++ Data/Item.cs
@@ -2,7 +2,7 @@
 
 namespace Baguettefy.Data
 {
-    public class Item
+    public partial class Item
     {
         [JsonProperty("ankama_id")]
         public long AnkamaId { get; set; }
@@ -10,8 +10,11 @@ namespace Baguettefy.Data
         [JsonProperty("name")]
         public string Name { get; set; }
 
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
         [JsonProperty("type")]
-        public TypeClass Type { get; set; }
+        public ItemType Type { get; set; }
 
         [JsonProperty("item_subtype")]
         public string ItemSubtype { get; set; }
@@ -19,11 +22,56 @@ namespace Baguettefy.Data
         [JsonProperty("level")]
         public long Level { get; set; }
 
+        [JsonProperty("pods")]
+        public long Pods { get; set; }
+
         [JsonProperty("image_urls")]
         public ImageUrls ImageUrls { get; set; }
+
+        [JsonProperty("effects")]
+        public Effect[] Effects { get; set; }
+
+        [JsonProperty("recipe")]
+        public Recipe[] Recipe { get; set; }
     }
 
-    public class ImageUrls
+    public partial class Effect
+    {
+        [JsonProperty("int_minimum")]
+        public long IntMinimum { get; set; }
+
+        [JsonProperty("int_maximum")]
+        public long IntMaximum { get; set; }
+
+        [JsonProperty("type")]
+        public EffectType Type { get; set; }
+
+        [JsonProperty("ignore_int_min")]
+        public bool IgnoreIntMin { get; set; }
+
+        [JsonProperty("ignore_int_max")]
+        public bool IgnoreIntMax { get; set; }
+
+        [JsonProperty("formatted")]
+        public string Formatted { get; set; }
+    }
+
+    public partial class EffectType
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("is_meta")]
+        public bool IsMeta { get; set; }
+
+        [JsonProperty("is_active")]
+        public bool IsActive { get; set; }
+    }
+
+    public partial class ImageUrls
     {
         [JsonProperty("icon")]
         public Uri Icon { get; set; }
@@ -38,7 +86,19 @@ namespace Baguettefy.Data
         public Uri Hd { get; set; }
     }
 
-    public class TypeClass
+    public partial class Recipe
+    {
+        [JsonProperty("item_ankama_id")]
+        public long ItemAnkamaId { get; set; }
+
+        [JsonProperty("item_subtype")]
+        public string ItemSubtype { get; set; }
+
+        [JsonProperty("quantity")]
+        public long Quantity { get; set; }
+    }
+
+    public partial class ItemType
     {
         [JsonProperty("name")]
         public string Name { get; set; }
--- RequestItemTranslateCommands.cs
+++ RequestItemTranslateCommands.cs
@@ -1,6 +1,11 @@
 ﻿using Discord.Interactions;
 using Baguettefy.Data;
+using Discord;
 using Newtonsoft.Json;
+using System.Reactive;
+using System.Reflection.Emit;
+using System.Security.Claims;
+using System.Xml.Linq;
 
 namespace Baguettefy
 {
@@ -8,65 +13,121 @@ namespace Baguettefy
     {
         static HttpClient client = new HttpClient();
 
-        [SlashCommand("baugettefy","Get the French name for a given English item's name.",runMode: RunMode.Async)]
+        [SlashCommand("baugettefy", "Get the French name for a given English item's name.", runMode: RunMode.Async)]
         public async Task Baugettefy(string name)
         {
             await DeferAsync(true);
-
-            var english_url = $"https://api.dofusdu.de/dofus2/en/items/search?query={name}&limit=1";
-
-            Item? item = null;
-            HttpResponseMessage response = await client.GetAsync(english_url);
-            if (response.IsSuccessStatusCode)
+            try
             {
-                string data = await response.Content.ReadAsStringAsync();
-                item = JsonConvert.DeserializeObject<Item[]>(data)?.FirstOrDefault();
-            }
-            
-            if (item == null)
-            {
-                await ModifyOriginalResponseAsync(properties =>
+
+                var english_search_url = $"https://api.dofusdu.de/dofus2/en/items/search?query={name}&limit=1";
+
+                Item? search_item = null;
+                HttpResponseMessage response = await client.GetAsync(english_search_url);
+                if (response.IsSuccessStatusCode)
                 {
-                    properties.Content = $"Sorry there was a problem, try again.";
-                });
-                return;
-            }
+                    string data = await response.Content.ReadAsStringAsync();
+                    search_item = JsonConvert.DeserializeObject<Item[]>(data)?.FirstOrDefault();
+                }
 
-            var itemType = item.ItemSubtype switch
-            {
-                "consumables" => "items/consumables",
-                "cosmetics" => "items/cosmetics",
-                "resources" => "items/resources",
-                "equipment" => "items/equipment",
-                "quest_items" => "items/quest_items",
-                "mounts" => "mounts",
-                "sets" => "sets",
-                _ => ""
-            };
-
-            var french_url = $"https://api.dofusdu.de/dofus2/fr/{itemType}/{item.AnkamaId}";
-            
-            Item? french_item = null;
-            HttpResponseMessage french_response = await client.GetAsync(french_url);
-            if (response.IsSuccessStatusCode)
-            {
-                string data = await french_response.Content.ReadAsStringAsync();
-                french_item = JsonConvert.DeserializeObject<Item>(data);
-            }
-            
-            if (french_item == null)
-            {
-                await ModifyOriginalResponseAsync(properties =>
+                if (search_item == null)
                 {
-                    properties.Content = $"Sorry there was a problem, try again.";
-                });
-                return;
-            }
+                    await ModifyOriginalResponseAsync(properties =>
+                    {
+                        properties.Content = $" \ud83e\udd56 Non non Baguette \ud83e\udd56\nSomething went wrong :(";
+                    });
+                    return;
+                }
 
-            await ModifyOriginalResponseAsync(properties =>
-            {
-                properties.Content = $" \ud83e\udd56 Oui Oui Baguette \ud83e\udd56: {french_item.Name}";
+                var itemType = search_item.ItemSubtype switch
+                {
+                    "consumables" => "items/consumables",
+                    "cosmetics" => "items/cosmetics",
+                    "resources" => "items/resources",
+                    "equipment" => "items/equipment",
+                    "quest" => "items/quest",
+                    "mounts" => "mounts",
+                    "sets" => "sets",
+                    _ => ""
+                };
+
+                var english_url = $"https://api.dofusdu.de/dofus2/en/{itemType}/{search_item.AnkamaId}";
+
+                Item? english_item = null;
+                HttpResponseMessage english_response = await client.GetAsync(english_url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = await english_response.Content.ReadAsStringAsync();
+                    english_item = JsonConvert.DeserializeObject<Item>(data);
+                }
+
+                if (english_item == null)
+                {
+                    await ModifyOriginalResponseAsync(properties =>
+                    {
+                        properties.Content = $" \ud83e\udd56 Non non Baguette \ud83e\udd56\nSomething went wrong :(";
+                    });
+                    return;
+                }
+
+
+                var french_url = $"https://api.dofusdu.de/dofus2/fr/{itemType}/{search_item.AnkamaId}";
+
+                Item? french_item = null;
+                HttpResponseMessage french_response = await client.GetAsync(french_url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = await french_response.Content.ReadAsStringAsync();
+                    french_item = JsonConvert.DeserializeObject<Item>(data);
+                }
+
+                if (french_item == null)
+                {
+                    await ModifyOriginalResponseAsync(properties =>
+                    {
+                        properties.Content = $" \ud83e\udd56 Non non Baguette \ud83e\udd56\nSomething went wrong :(";
+                    });
+                    return;
+                }
+
+                var embedBuilder = new EmbedBuilder()
+                {
+                    Title = $"{french_item.Name}",
+                    ThumbnailUrl = french_item.ImageUrls.Icon?.AbsoluteUri,
+                };
+
+                embedBuilder.WithFields(new[]
+                {
+                new EmbedFieldBuilder()
+                    .WithName($"Name")
+                    .WithValue($"{english_item.Name}")
+                    .WithIsInline(false),
+
+                new EmbedFieldBuilder()
+                    .WithName($"Category")
+                    .WithValue($"{search_item.ItemSubtype}")
+                    .WithIsInline(false),
+
+                new EmbedFieldBuilder()
+                    .WithName($"Description")
+                    .WithValue($"{english_item.Description}")
+                    .WithIsInline(false)
+
             });
+
+                await ModifyOriginalResponseAsync(properties =>
+                {
+                    properties.Content = $" \ud83e\udd56 Oui Oui Baguette \ud83e\udd56";
+                    properties.Embed = embedBuilder.Build();
+                });
+            }
+            catch (Exception e)
+            {
+                await ModifyOriginalResponseAsync(properties =>
+                {
+                    properties.Content = $" \ud83e\udd56 Non non Baguette \ud83e\udd56\nSomething went wrong :(";
+                });
+            }
 
         }
     }
