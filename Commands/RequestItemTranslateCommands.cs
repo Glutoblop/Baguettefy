c9f37de8d35fefae65e038cea3672b13b2911569
jordan nye <jordannye@hotmail.com>
2025-03-13 20:46:13 +0000

    WIP updating translate item command

--- Commands/RequestItemTranslateCommands.cs
+++ Commands/RequestItemTranslateCommands.cs
@@ -1,14 +1,10 @@
-﻿using Baguettefy.Data.Items;
-using Discord;
+﻿using Discord;
 using Discord.Interactions;
-using Newtonsoft.Json;
 
 namespace Baguettefy.Commands
 {
     public class RequestItemTranslateCommands : InteractionModuleBase<InteractionContext>
     {
-        static readonly HttpClient _Client = new HttpClient();
-
         [SlashCommand("translate_item", "Search the name of an item either French/English, return info.", runMode: RunMode.Async)]
         public async Task GetItemName(string name)
         {
@@ -16,103 +12,22 @@ namespace Baguettefy.Commands
 
             try
             {
-                string[] searches = new string[]
+                EmbedBuilder embedBuilder = await FindTranslationData.FindItem(name);
+                if (embedBuilder != null)
                 {
-                    $"https://api.dofusdu.de/dofus2/fr/items/search?query={name}&limit=1",
-                    $"https://api.dofusdu.de/dofus2/en/items/search?query={name}&limit=1"
-                };
-
-                ItemData? searchData = null;
-                HttpResponseMessage response = null;
-
-                for (int i = 0; i < searches.Length; i++)
-                {
-                    response = await _Client.GetAsync(searches[i]);
-                    if (response.IsSuccessStatusCode)
+                    await ModifyOriginalResponseAsync(properties =>
                     {
-                        string data = await response.Content.ReadAsStringAsync();
-                        searchData = JsonConvert.DeserializeObject<ItemData[]>(data)?.FirstOrDefault();
-                    }
+                        properties.Content = $" \ud83e\udd56 Oui Oui Baguette \ud83e\udd56";
+                        properties.Embed = embedBuilder.Build();
+                    });
                 }
-
-                if (searchData == null)
+                else
                 {
                     await ModifyOriginalResponseAsync(properties =>
                     {
                         properties.Content = $" \ud83e\udd56 Non non Baguette \ud83e\udd56\nSomething went wrong :(";
                     });
-                    return;
                 }
-
-                var itemType = searchData.ItemSubtype switch
-                {
-                    "consumables" => "items/consumables",
-                    "cosmetics" => "items/cosmetics",
-                    "resources" => "items/resources",
-                    "equipment" => "items/equipment",
-                    "quest" => "items/quest",
-                    "mounts" => "mounts",
-                    "sets" => "sets",
-                    _ => ""
-                };
-
-                var english_detail_url = $"https://api.dofusdu.de/dofus2/en/{itemType}/{searchData.AnkamaId}";
-                ItemData? english_item = null;
-                HttpResponseMessage english_detail_response = await _Client.GetAsync(english_detail_url);
-                if (english_detail_response.IsSuccessStatusCode)
-                {
-                    string data = await english_detail_response.Content.ReadAsStringAsync();
-                    english_item = JsonConvert.DeserializeObject<ItemData>(data);
-                }
-
-                if (english_item == null)
-                {
-                    await ModifyOriginalResponseAsync(properties =>
-                    {
-                        properties.Content = $" \ud83e\udd56 Non non Baguette \ud83e\udd56\nSomething went wrong :(";
-                    });
-                    return;
-                }
-
-                var french_detail_url = $"https://api.dofusdu.de/dofus2/fr/{itemType}/{searchData.AnkamaId}";
-                ItemData? french_item = null;
-                HttpResponseMessage french_detail_response = await _Client.GetAsync(french_detail_url);
-                if (french_detail_response.IsSuccessStatusCode)
-                {
-                    string data = await french_detail_response.Content.ReadAsStringAsync();
-                    french_item = JsonConvert.DeserializeObject<ItemData>(data);
-                }
-
-                if (french_item == null)
-                {
-                    await ModifyOriginalResponseAsync(properties =>
-                    {
-                        properties.Content = $" \ud83e\udd56 Non non Baguette \ud83e\udd56\nSomething went wrong :(";
-                    });
-                    return;
-                }
-
-                EmbedBuilder embedBuilder = EmbedCreator.CreateTranslatedEmbed($"", english_item.Name, french_item.Name);
-                embedBuilder.Title = $"Search: '{name}' found..";
-                embedBuilder.ThumbnailUrl = english_item.ImageUrls.Icon?.AbsoluteUri;
-                embedBuilder.AddField(
-                    new EmbedFieldBuilder()
-                    .WithName($"Category")
-                    .WithValue($"{(english_item?.Type?.Name ?? "Unknown")}")
-                    .WithIsInline(false)
-                );
-                embedBuilder.AddField(
-                    new EmbedFieldBuilder()
-                    .WithName($"Description")
-                    .WithValue($"{(english_item?.Description ?? "Unknown")}")
-                    .WithIsInline(false)
-                );
-
-                await ModifyOriginalResponseAsync(properties =>
-                {
-                    properties.Content = $" \ud83e\udd56 Oui Oui Baguette \ud83e\udd56";
-                    properties.Embed = embedBuilder.Build();
-                });
             }
             catch (Exception e)
             {
--- FindTranslationData.cs
+++ FindTranslationData.cs
@@ -1,13 +1,17 @@
 ﻿using Baguettefy.Core.Interfaces;
 using Baguettefy.Data.DofusDb.Achievements;
 using Baguettefy.Data.DofusDb.Dungeons;
+using Baguettefy.Data.Items;
 using Baguettefy.Data.Quests;
 using Discord;
+using Newtonsoft.Json;
 
 namespace Baguettefy
 {
     public class FindTranslationData
     {
+        static readonly HttpClient _Client = new HttpClient();
+
         public static async Task<EmbedBuilder> FindQuest(IFirebaseDatabase db, string name)
         {
             QuestData? foundQuest = null;
@@ -75,6 +79,90 @@ namespace Baguettefy
             });
             if (foundDungeon == null) return null;
             return EmbedCreator.CreateTranslatedEmbed("Dungeon", foundDungeon.Name.En, foundDungeon.Name.Fr);
+        }
+
+        internal static async Task<EmbedBuilder> FindItem(string name)
+        {
+            string[] searches = new string[]
+                {
+                    $"https://api.dofusdu.de/dofus2/fr/items/search?query={name}&limit=1",
+                    $"https://api.dofusdu.de/dofus2/en/items/search?query={name}&limit=1"
+                };
+
+            ItemData? searchData = null;
+            HttpResponseMessage response = null;
+
+            for (int i = 0; i < searches.Length; i++)
+            {
+                response = await _Client.GetAsync(searches[i]);
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = await response.Content.ReadAsStringAsync();
+                    searchData = JsonConvert.DeserializeObject<ItemData[]>(data)?.FirstOrDefault();
+                }
+            }
+
+            if (searchData == null)
+            {
+                return null;
+            }
+
+            var itemType = searchData.ItemSubtype switch
+            {
+                "consumables" => "items/consumables",
+                "cosmetics" => "items/cosmetics",
+                "resources" => "items/resources",
+                "equipment" => "items/equipment",
+                "quest" => "items/quest",
+                "mounts" => "mounts",
+                "sets" => "sets",
+                _ => ""
+            };
+
+            var english_detail_url = $"https://api.dofusdu.de/dofus2/en/{itemType}/{searchData.AnkamaId}";
+            ItemData? english_item = null;
+            HttpResponseMessage english_detail_response = await _Client.GetAsync(english_detail_url);
+            if (english_detail_response.IsSuccessStatusCode)
+            {
+                string data = await english_detail_response.Content.ReadAsStringAsync();
+                english_item = JsonConvert.DeserializeObject<ItemData>(data);
+            }
+
+            if (english_item == null)
+            {
+                return null;
+            }
+
+            var french_detail_url = $"https://api.dofusdu.de/dofus2/fr/{itemType}/{searchData.AnkamaId}";
+            ItemData? french_item = null;
+            HttpResponseMessage french_detail_response = await _Client.GetAsync(french_detail_url);
+            if (french_detail_response.IsSuccessStatusCode)
+            {
+                string data = await french_detail_response.Content.ReadAsStringAsync();
+                french_item = JsonConvert.DeserializeObject<ItemData>(data);
+            }
+
+            if (french_item == null)
+            {
+                return null;
+            }
+
+            EmbedBuilder embedBuilder = EmbedCreator.CreateTranslatedEmbed($"", english_item.Name, french_item.Name);
+            embedBuilder.Title = $"Search: '{name}' found..";
+            embedBuilder.ThumbnailUrl = english_item.ImageUrls.Icon?.AbsoluteUri;
+            embedBuilder.AddField(
+                new EmbedFieldBuilder()
+                .WithName($"Category")
+                .WithValue($"{(english_item?.Type?.Name ?? "Unknown")}")
+                .WithIsInline(false)
+            );
+            embedBuilder.AddField(
+                new EmbedFieldBuilder()
+                .WithName($"Description")
+                .WithValue($"{(english_item?.Description ?? "Unknown")}")
+                .WithIsInline(false)
+            );
+            return embedBuilder;
         }
     }
 }
